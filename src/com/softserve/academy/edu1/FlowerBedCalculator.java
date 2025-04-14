8d08c25aaf5623b432a12d9a6d1f253018aeb1ef
Arterium <arterium13@gmail.com>
2025-04-14 17:04:31 +0300

    add FlowerBedCalculator add UserInfo add PhoneCallCostCalculator

--- src/com/softserve/academy/edu1/FlowerBedCalculator.java
+++ src/com/softserve/academy/edu1/FlowerBedCalculator.java
@@ -1,4 +1,19 @@
 package softserve.academy.edu1;
 
+import java.io.Console;
+import java.util.Scanner;
+
 public class FlowerBedCalculator {
+    public static void main(String[] args) {
+        int radius;
+        double perimeter;
+        double area;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" input the radius of the flower bed");
+        radius = scanner.nextInt();
+        perimeter = 2 * Math.PI * radius;
+        area = Math.PI * radius * radius;
+        System.out.println("Flowerbed perimeter = " + perimeter);
+        System.out.println("Flowerbed area = " + area);
+    }
 }
