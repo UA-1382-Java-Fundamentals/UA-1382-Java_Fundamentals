358472752be1ebde55eee14c262eb2940b1f0109
Artemii Kostariev <xiradice@gmail.com>
2025-04-16 14:57:35 +0300

    Now used different method for input. Getter and Setter now in use.

--- src/com/softserve/academy/edu1/homework3/person/PersonDemo.java
+++ src/com/softserve/academy/edu1/homework3/person/PersonDemo.java
@@ -14,18 +14,23 @@ public class PersonDemo {
         person1.input(scanner);
         person1.changeName("Artemii", "Kostariev");
         person1.output();
-        System.out.println("Enter data for person 2: ");
-        person2.input(scanner);
+        System.out.println("Data for person 2 already exist: ");
+        person2.setFirstName("Dmytro");
+        person2.setLastName("Sergienko");
+        person2.setBirthYear(1990);
         person2.output();
         System.out.println("Enter data for person 3: ");
-        person3.input(scanner);
+        System.out.println("Enter first name: ");
+        person3.setFirstName(scanner.nextLine());
+        System.out.println("Enter last name: ");
+        person3.setLastName(scanner.nextLine());
         person3.output();
         System.out.println("Enter data for person 4: ");
         person4.input(scanner);
         person4.output();
         System.out.println("Enter data for person 5: ");
         person5.input(scanner);
-        person5.output();
+        System.out.println("Person 5: " + person5.getFirstName() + " " + person5.getLastName() + ", age: " + person5.getAge());
         scanner.close();
     }
 }
