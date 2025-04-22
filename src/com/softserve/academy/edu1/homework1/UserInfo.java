f74ea9966aa539b1e806208d5e0fe25ddd7b4ac9
L0w-uP <halushchakostap@gmail.com>
2025-04-22 09:34:20 +0200

    Homework 1

---
+++ src/com/softserve/academy/edu1/homework1/UserInfo.java
@@ -0,0 +1,16 @@
+package com.softserve.academy.edu1.homework1;
+
+import java.util.Scanner;
+
+public class UserInfo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = sc.nextLine();
+        System.out.println("Where do you live " + name + "?");
+        String address = sc.nextLine();
+        sc.close();
+
+        System.out.println("Name: " + name + "\nAddress: " + address);
+    }
+}
