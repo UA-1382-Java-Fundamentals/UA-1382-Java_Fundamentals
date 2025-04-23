8fd909dca8dc55cfc267a7a38fad7aed7ba0f272
Svetika <sveetochka@gmail.com>
2025-04-23 09:54:34 +0300

    Added Homework 3, task 2

---
+++ src/com/softserve/academy/hw3_t2/Main.java
@@ -0,0 +1,27 @@
+/*
+Write Java console application with method to find the smallest number among three integer numbers. Example of
+output:
+
+Input of the first number: 18.0
+Input of the second number: 32.0
+Input the third number: 54.0
+The smallest number is 18.0
+ */
+
+package com.softserve.academy.hw3_t2;
+
+public class Main {
+    public static void main(String[] args) {
+        Numbers obj = new Numbers();
+
+        obj.setNumber1(obj.getNumber("Input Number 1:"));
+        obj.setNumber2(obj.getNumber("Input Number 2:"));
+        obj.setNumber3(obj.getNumber("Input Number 3:"));
+
+        obj.printResults("Input Number 1: ", obj.getNumber1());
+        obj.printResults("Input Number 2: ", obj.getNumber2());
+        obj.printResults("Input Number 3: ", obj.getNumber3());
+        obj.printResults("The smallest number is ", obj.findTheSmallestNumber());
+
+    }
+}
---
+++ src/com/softserve/academy/hw3_t2/Numbers.java
@@ -0,0 +1,52 @@
+package com.softserve.academy.hw3_t2;
+
+import java.util.Scanner;
+
+public class Numbers {
+    private double number1;
+    private double number2;
+    private double number3;
+
+    public double getNumber1() {
+        return number1;
+    }
+
+    public double getNumber2() {
+        return number2;
+    }
+
+    public double getNumber3() {
+        return number3;
+    }
+
+    public void setNumber1(double number1) {
+        this.number1 = number1;
+    }
+
+    public void setNumber2(double number2) {
+        this.number2 = number2;
+    }
+
+    public void setNumber3(double number3) {
+        this.number3 = number3;
+    }
+
+    public double getNumber(String message) {
+        System.out.println(message);
+        Scanner sc = new Scanner(System.in);
+        return sc.nextDouble();
+    }
+
+    public double findTheSmallestNumber() {
+        if (number1 <= number2 & number1 <= number3)  {
+            return number1;
+        } else if (number2 <= number1 & number2 <= number3) {
+            return number2;
+        }
+        else return number3;
+    }
+
+    void printResults(String text, double value) {
+        System.out.println(text + value);
+    }
+}
