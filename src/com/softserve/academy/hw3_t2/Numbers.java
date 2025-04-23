211023d878983cf4e4f444387cc3696a9d6af7f7
Svetika <sveetochka@gmail.com>
2025-04-23 15:05:58 +0300

    Updated Homework 3 task 2

--- src/com/softserve/academy/hw3_t2/Numbers.java
+++ src/com/softserve/academy/hw3_t2/Numbers.java
@@ -38,9 +38,9 @@ public class Numbers {
     }
 
     public double findTheSmallestNumber() {
-        if (number1 <= number2 & number1 <= number3)  {
+        if (number1 <= number2 && number1 <= number3)  {
             return number1;
-        } else if (number2 <= number1 & number2 <= number3) {
+        } else if (number2 <= number1 && number2 <= number3) {
             return number2;
         }
         else return number3;
