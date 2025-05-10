d634433099c71e0faa0e5bdccf18c64381a893f4
sandiukp <sandiukp@gmail.com>
2025-05-10 10:20:32 +0300

    add homeWork 9

--- src/main/java/com/softserve/academy/homework9/practicalTask1/Department.java
+++ src/main/java/com/softserve/academy/homework9/practicalTask1/Department.java
@@ -1,5 +1,60 @@
 package com.softserve.academy.homework9.practicalTask1;
 
-public class Department {
 
+public class Department  implements Cloneable{
+    private String name;
+    Address address;
+
+
+    public Department(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
+    static class Address implements Cloneable{
+       String city;
+       String street;
+       int buildingNumber;
+
+       public Address(String city, String street, int buildingNumber) {
+           this.city = city;
+           this.street = street;
+           this.buildingNumber = buildingNumber;
+       }
+
+        @Override
+        public Address clone() {
+            try {
+                return (Address) super.clone();
+            } catch (CloneNotSupportedException e) {
+                return null;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "city='" + city + '\'' +
+                    ", street='" + street + '\'' +
+                    ", buildingNumber=" + buildingNumber +
+                    '}';
+        }
+    }
+
+    @Override
+    public Department clone() {
+        try {
+            var res = (Department) super.clone();
+            res.address = address.clone();
+            return res;
+        } catch (CloneNotSupportedException e) {
+            return null;
+        }
+    }
 }
--- src/main/java/com/softserve/academy/homework9/practicalTask1/Task1.java
+++ src/main/java/com/softserve/academy/homework9/practicalTask1/Task1.java
@@ -1,4 +1,18 @@
 package com.softserve.academy.homework9.practicalTask1;
 
 public class Task1 {
+    public static void main(String[] args) {
+        Department departmentOrig = new Department("IT",
+                new Department.Address("Lviv", "Chervonoyi Kalyny", 1));
+        Department departmentClone = departmentOrig.clone();
+
+        System.out.println("DepartmentOrigin" + departmentOrig);
+        System.out.println("DepartmentClone"  + departmentClone);
+
+        departmentClone.address.city = "Kyiv";
+
+        System.out.println("DepartmentOrigin" + departmentOrig);
+        System.out.println("DepartmentClone"  + departmentClone);
+    }
 }
+
