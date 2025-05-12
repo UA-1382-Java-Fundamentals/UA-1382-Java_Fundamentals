7b07505162555cf699bc724bd02d9a80f5f6e427
Svetika <sveetochka@gmail.com>
2025-05-12 21:02:01 +0200

    Update Homework 4 task 4

--- src/com/softserve/academy/hw4_t4/Main.java
+++ src/com/softserve/academy/hw4_t4/Main.java
@@ -11,17 +11,19 @@ package com.softserve.academy.hw4_t4;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog1 = new Dog("Carlos", 12, Dog.Breed.HUSKY);
-        Dog dog2 = new Dog("Ava", 1, Dog.Breed.LABRADOR);
-        Dog dog3 = new Dog("Ava", 8, Dog.Breed.GOLDEN_RETRIEVER);
+        Dog[] dogs = {
+                new Dog("Carlos", 12, Dog.Breed.HUSKY),
+                new Dog("Ava", 1, Dog.Breed.LABRADOR),
+                new Dog("Max", 8, Dog.Breed.GOLDEN_RETRIEVER)
+        };
 
-        dog1.getOldestAge(dog1.getAge());
-        dog2.getOldestAge(dog2.getAge());
-        dog3.getOldestAge(dog3.getAge());
+        for (Dog dog : dogs) {
+            dog.getOldestAge(dog.getAge());
+        }
 
-        dog1.printOldestDog();
-        dog2.printOldestDog();
-        dog3.printOldestDog();
+        for (Dog dog : dogs) {
+            dog.printOldestDog();
+        }
 
     }
 }
