17f34eed78d1dd8e0a1c95962bdd988ac34bc89a
Svetika <sveetochka@gmail.com>
2025-05-15 00:16:54 +0200

    Updated Homework 5 task 1

--- src/com/softserve/academy/hw5_t1/Month.java
+++ src/com/softserve/academy/hw5_t1/Month.java
@@ -50,6 +50,6 @@ public class Month {
             MonthAndDays = new int[]{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
         }
 
-        System.out.println(MonthAndDays.length);
+        System.out.println("Number of days in month: " + MonthAndDays.length);
     }
 }
