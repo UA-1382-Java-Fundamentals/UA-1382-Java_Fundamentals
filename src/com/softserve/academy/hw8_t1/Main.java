baeaad2304e427f466e790cceef65b40fe87403d
Svetika <sveetochka@gmail.com>
2025-05-18 13:40:53 +0200

    Added Homework 8 task 1

---
+++ src/com/softserve/academy/hw8_t1/Main.java
@@ -0,0 +1,15 @@
+/*
+Create div() method, which calculates the dividing of two double type
+numbers. In main() method input 2 double numbers and call this method.
+Catch all exceptions.
+ */
+
+package com.softserve.academy.hw8_t1;
+
+public class Main {
+    public static void main(String[] args) {
+        TwoNumbers twoNumbers = new TwoNumbers();
+        twoNumbers.inputNumbers();
+        twoNumbers.printResults();
+    }
+}
---
+++ src/com/softserve/academy/hw8_t1/TwoNumbers.java
@@ -0,0 +1,51 @@
+package com.softserve.academy.hw8_t1;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TwoNumbers {
+    private final double[] numbers = new double[2];
+    private double division;
+    private boolean exit;
+
+    public void inputNumbers() {
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < numbers.length; i++) {
+            try {
+                System.out.println("Input a number (if not integer, use a point separator)");
+                numbers[i] = sc.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Not a number");
+                exit = true;
+            }
+            if (exit) {
+                break;
+            }
+        }
+        sc.close();
+    }
+
+    public void div() throws ArithmeticException  {
+        if (Double.isInfinite(numbers[0] / numbers[1])) {
+            throw new ArithmeticException();
+        } else {
+            division = numbers[0] / numbers[1];
+        }
+    }
+
+    public void printResults() {
+        if (exit) {
+            return;
+        } else {
+            try {
+                div();
+                System.out.println(division);
+            } catch (ArithmeticException e) {
+                System.out.println("Division by zero");
+            } catch (Exception e) {
+                System.out.println("Unknown error");
+            }
+        }
+    }
+}
