e766bc29f2532cc9b78253b6dcc9d810b2ee8765
Svetika <sveetochka@gmail.com>
2025-05-18 16:12:50 +0200

    Updated Homework 8 task 1

--- src/com/softserve/academy/hw8_t1/TwoNumbers.java
+++ src/com/softserve/academy/hw8_t1/TwoNumbers.java
@@ -18,11 +18,10 @@ public class TwoNumbers {
             } catch (InputMismatchException e) {
                 System.out.println("Not a number");
                 exit = true;
+                break;
             } catch (Exception e) {
                 System.out.println("Unknown error");
-            }
-
-            if (exit) {
+                exit = true;
                 break;
             }
         }
