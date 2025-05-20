6cd5a884b95df4a723d40645e4e940a4980b5dac
Serhii Manyk <manukserg1988@gmail.com>
2025-05-20 22:52:55 +0200

    task09 and task10 homework

--- src/main/java/Task09/PracticalTask01/Main.java
+++ src/main/java/Task09/PracticalTask01/Main.java
@@ -5,21 +5,21 @@ public class Main {
     public static void main(String[] args){
         Department dep1 = new Department("Centr");
         Department.Addres adres1 =dep1.new Addres("Warszawa","Pruszkowska",123);
-
+        Department.Addres copyAdres1;
         try {
-         Department.Addres copyAdres1 = (Department.Addres) adres1.clone();
-            System.out.println("Before changes");
-            System.out.println(adres1.toString());
-            System.out.println(copyAdres1.toString());
-
-            copyAdres1.setBuilding(666);
-            copyAdres1.setCity("Krakow");
-
-            System.out.println("=*=".repeat(15));
-            System.out.println(adres1.toString());
-            System.out.println(copyAdres1.toString());
+            copyAdres1 = (Department.Addres) adres1.clone();
         } catch (CloneNotSupportedException e) {
            throw new RuntimeException(e);
         }
+        System.out.println("Before changes");
+        System.out.println(adres1.toString());
+        System.out.println(copyAdres1.toString());
+
+        copyAdres1.setBuilding(666);
+        copyAdres1.setCity("Krakow");
+
+        System.out.println("=*=".repeat(15));
+        System.out.println(adres1.toString());
+        System.out.println(copyAdres1.toString());
     }
 }
