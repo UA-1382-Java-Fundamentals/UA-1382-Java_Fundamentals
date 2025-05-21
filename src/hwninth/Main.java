4f238f7fc4e2d5d0b77bbc32bdda33bdefd781f4
nata <natakush.job@gmail.com>
2025-05-21 21:13:25 +0300

    UPD hw for lection9 kushpeta

--- src/hwninth/Main.java
+++ src/hwninth/Main.java
@@ -2,9 +2,9 @@ package hwninth;
 
 public class Main {
     public static void main(String[] args) {
-        Person student = new Teacher("Alex", "Kodin", 45, "KNU");
+        Person teacher = new Teacher("Alex", "Kodin", 45, "KNU");
 
-        System.out.println(student.info());
-        System.out.println(student.activity());
+        System.out.println(teacher.info());
+        System.out.println(teacher.activity());
     }
 }
